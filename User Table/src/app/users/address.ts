0c9b25266958f4b2d7e61cbd0de985941e8c894d
Sanjana Madhulika <37425149+sanjanamadhulika@users.noreply.github.com>
2018-12-02 17:40:22 -0500

    Removed duplicated project

--- User Table/src/app/users/address.ts
+++
@@ -1,9 +0,0 @@
-import { IGeoLocation } from "./geolocation";
-
-export interface IAddress {
-  street: string;
-  suite: string;
-  city: string;
-  zipcode: string;
-  geo: IGeoLocation;
-}
--- User Table/src/app/users/geolocation.ts
+++
@@ -1,4 +0,0 @@
-export interface IGeoLocation {
-  latitude: string;
-  longitude: string;
-}
